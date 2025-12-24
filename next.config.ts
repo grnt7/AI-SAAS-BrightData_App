8112345badcdf662cfc593d35d8e97d46b366c6c
David <grantclan.7@gmail.com>
2025-12-24 10:48:32 +0000

    Removed invalid devServer key for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
  
-  devServer: {
-    allowedDevOrigins: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'http://169.254.27.46:3000', // Explicitly allow the detected IP
-    ],
-  },
+  // devServer: {
+  //   allowedDevOrigins: [
+  //     'http://localhost:3000',
+  //     'http://127.0.0.1:3000',
+  //     'http://169.254.27.46:3000', // Explicitly allow the detected IP
+  //   ],
+  // },
 };
 
 export default nextConfig;

96ef08d97f04c20fee8c2a7bd0398dce3bbfac4a
David <grantclan.7@gmail.com>
2025-12-24 11:41:44 +0000

    next.config.ts update

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
  
-  // devServer: {
-  //   allowedDevOrigins: [
-  //     'http://localhost:3000',
-  //     'http://127.0.0.1:3000',
-  //     'http://169.254.27.46:3000', // Explicitly allow the detected IP
-  //   ],
-  // },
+ 
   eslint: {
     // This allows the build to finish even if you have apostrophe errors
     ignoreDuringBuilds: true,
